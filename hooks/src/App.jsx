db689dd579d286d7e3c93ce034b3aadac9a31257
Maaz Ul Haq <maazulhaq00@gmail.com>
2025-04-17 18:29:16 +0500

    working with APIs

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -2,6 +2,8 @@ import DigitalClock from "./pages/DigitalClock";
 import PostsAPI from "./pages/PostsAPI";
 import ReactUseEffect from "./pages/ReactUseEffect";
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 function App() {
   return (<>
     {/* <ReactUseEffect />
--- hooks/src/pages/PostsAPI.jsx
+++ hooks/src/pages/PostsAPI.jsx
@@ -4,16 +4,41 @@ function PostsAPI() {
 
     const [posts, setPosts] = useState([])
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch("https://jsonplaceholder.typicode.com/posts")
-        .then((res) => res.json())
-        .then((data) => setPosts(data))
-        .catch((err) => console.log(err))
+            .then((res) => res.json())
+            .then((data) => setPosts(data))
+            .catch((err) => console.log(err))
     }, []);
 
 
     return (<>
-    <h1>Posts</h1>
+        <h1>Posts</h1>
+
+        <div className="row row-cols-1 row-cols-md-4 g-4" >
+
+            {
+                posts.map((item) => {
+                    return (
+                        <div className="col" key={item.id}>
+                            <div className="card">
+
+                                <div className="card-body">
+                                    <h5 className="card-title">{item.title}</h5>
+                                    <p className="card-text">
+                                        {item.body}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+
+
+
+        </div>
+
     </>);
 }
 
